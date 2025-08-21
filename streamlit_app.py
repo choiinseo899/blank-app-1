ff45b7a5b18920842c8f8aae37408e28c3f9c113
choiinseo899 <d2518@e-mirim.hs.kr>
2025-08-21 00:12:38 +0000

    01

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title("🎈 My new app")
+st.title("📚 우리반 시간표")
 st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+    "우리반의 시간표를 확인해보세요!"
 )
